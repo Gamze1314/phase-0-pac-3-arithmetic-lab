4382a869b1f01e7d2eb2b523a57c378b3905b200
gamze <gamzekandara@gmail.com>
2023-10-26 15:14:45 -0400

    Adds index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+function add() {
+  return "something";
+}
+function add(a,b) { 
+  return (a + b); 
+}
+function subtract() {
+   return "someting";
+}
+function subtract(b,a) {
+   return (b - a);
+}
+
+function multiply() {
+return "something"; 
+}
+function multiply (a,b) {
+    return (a*b);
+}
+
+function divide() {
+ return "something"; 
+}
+function divide(a,b) {
+    return ( a/= b);
+}
+function increment(n) {
+return (n + 1);
+}
+
+function decrement(n) {
+    return (n-1);
+}
+function makeInt(n) {
+    return parseInt(n,10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
